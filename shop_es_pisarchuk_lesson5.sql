fbe87bab818b2454bb1fe71462493ca909ba8fe1
PISEV <es_pisachuk@mail.ru>
2022-04-08 11:17:13 +0300

    UPDATE users SET created_at = NOW(), updated_at = current_timestamp();
    ALTER TABLE users MODIFY created_at varchar(30);
    ALTER TABLE users MODIFY updated_at varchar(30);
    UPDATE users SET created_at = '20.10.2017 8:10';
    UPDATE users SET updated_at = '20.10.2017 8:10';
    
    UPDATE users SET created_at = CONCAT(SUBSTR(created_at, 7, 4), '-', SUBSTR(created_at, 4, 2), '-', SUBSTR(created_at, 1, 2),' ',SUBSTR(created_at, 12, 1),':',SUBSTR(created_at, 14, 2),':00');
    UPDATE users SET updated_at = CONCAT(SUBSTR(updated_at, 7, 4), '-', SUBSTR(updated_at, 4, 2), '-', SUBSTR(updated_at, 1, 2),' ',SUBSTR(updated_at, 12, 1),':',SUBSTR(updated_at, 14, 2),':00');
    ALTER TABLE users MODIFY created_at datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
    ALTER TABLE users MODIFY updated_at datetime DEFAULT CURRENT_TIMESTAMP;
    
    create database if not exists shop;
    use shop;
    DROP TABLE IF EXISTS storehouses_products;
    CREATE TABLE storehouses_products (
      id SERIAL PRIMARY KEY,
      storehouse_id INT UNSIGNED,
      product_id INT UNSIGNED,
      value INT UNSIGNED COMMENT 'Запас товарной позиции на складе',
      created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
      updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
    ) COMMENT = 'Запасы на складе';
    
    Слава GeekBrains!

---
+++ shop_es_pisarchuk_lesson5.sql
@@ -0,0 +1,56 @@
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: shop
+-- ------------------------------------------------------
+-- Server version	8.0.28
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `storehouses_products`
+--
+
+DROP TABLE IF EXISTS `storehouses_products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `storehouses_products` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `storehouse_id` int unsigned DEFAULT NULL,
+  `product_id` int unsigned DEFAULT NULL,
+  `value` int unsigned DEFAULT NULL COMMENT 'Запас товарной позиции на складе',
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Запасы на складе';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `storehouses_products`
+--
+
+LOCK TABLES `storehouses_products` WRITE;
+/*!40000 ALTER TABLE `storehouses_products` DISABLE KEYS */;
+INSERT INTO `storehouses_products` VALUES (1,1,18,0,'2022-04-08 10:23:36','2022-04-08 10:23:36'),(2,3,19,25,'2022-04-08 10:23:36','2022-04-08 10:23:36'),(3,4,23,0,'2022-04-08 10:23:36','2022-04-08 10:23:36'),(4,7,2,0,'2022-04-08 10:23:36','2022-04-08 10:23:36'),(5,14,3,35,'2022-04-08 10:23:36','2022-04-08 10:23:36'),(6,16,15,500,'2022-04-08 10:23:36','2022-04-08 10:23:36'),(7,18,19,1000,'2022-04-08 10:23:36','2022-04-08 10:23:36'),(8,23,21,3000,'2022-04-08 10:23:36','2022-04-08 10:23:36');
+/*!40000 ALTER TABLE `storehouses_products` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-04-08 11:10:24
